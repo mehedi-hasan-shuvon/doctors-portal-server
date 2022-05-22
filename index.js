a517476b0e00f1548ffe967003c2d5eecc865c5a
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-22 11:55:45 +0600

    check admin role and complete userCollection

--- index.js
+++ index.js
@@ -66,12 +66,29 @@ async function run() {
         //handel and update social users
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            } else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        });
+
+        //check if a user admin or not
+        app.get('/admin/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+
         });
 
 
